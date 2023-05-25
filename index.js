d8b5d4f4c1e826f3a5307430e334f886a1624ac8
Lb Madesia <lb.madesia@viitor.cloud>
2023-05-25 11:18:57 +0530

    chrome extension

---
+++ index.js
@@ -0,0 +1,34 @@
+// async function fetchData() {
+//     const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+//     const record=await res.json();
+//     document.getElementById("date").innerHTML=record.data[0].date;
+//     document.getElementById("areaName").innerHTML=record.data[0].areaName;
+//     document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+//     document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+
+// }
+
+// Function to get the Selected Text
+document.getElementById("clickBtn").onclick = function () {
+  getSelectedText();
+};
+
+function getSelectedText() {
+  var selectedText = "";
+
+  // window.getSelection
+  if (window.getSelection) {
+    selectedText = window.getSelection();
+  }
+  // document.getSelection
+  else if (document.getSelection) {
+    selectedText = document.getSelection();
+  }
+  // document.selection
+  else if (document.selection) {
+    selectedText = document.selection.createRange().text;
+  } else return;
+  // To write the selected text into the textarea
+  document.getElementById("textTag").innerHTML = selectedText;
+}
+// fetchData();
